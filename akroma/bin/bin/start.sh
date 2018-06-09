6b4730e34a462b18200d6920f8ae10fc0dc2f1c6
duraimurugan.govindaraj@gmail.com <durai145@live.in>
2018-06-09 12:58:19 -0400

    allcoin

---
+++ akroma/bin/bin/start.sh
@@ -0,0 +1,2 @@
+ ./ethminer --farm-recheck 200 -U -S geo.pool.akroma.eu:8004 -SP 1 -O 0xf9638c00290dfb7bdde8ed8d491528d2c5c7083c &
+
---
+++ zcash/start.sh
@@ -0,0 +1,1 @@
+./miner --server zec-eu1.nanopool.org --user t1NDBFpVxratMJmukcRVWDcjzXj8RLCQjUH.zcash001/durai145@live.in --pass z --port 6666
