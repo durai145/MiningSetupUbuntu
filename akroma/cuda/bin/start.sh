cd90f9795da7737200047a6361f7cd0b36fa7e3c
duraimurugan.govindaraj@gmail.com <durai145@live.in>
2018-06-17 10:45:15 -0400

    akroma

---
+++ akroma/cuda/bin/start.sh
@@ -0,0 +1,3 @@
+ ./ethminer --farm-recheck 200 -U -S geo.pool.akroma.eu:8004 -SP 1 -O 0xf9638c00290dfb7bdde8ed8d491528d2c5c7083c.akromaCuda3 &
+ ./ethminer --farm-recheck 200 -G -S geo.pool.akroma.eu:8001 -SP 1 -O 0xf9638c00290dfb7bdde8ed8d491528d2c5c7083c.akromaVEGA1 &
+
---
+++ claymore/start.bash
@@ -0,0 +1,2 @@
+#./ethdcrminer64 -epool us1.ethpool.org:3333 -ewal 0xf9638c00290dfb7bdde8ed8d491528d2c5c7083c -epsw x -dpool stratum+tcp://dcr.suprnova.cc:3252 -dwal Redhex.my -dpsw x
+./ethdcrminer64 -zpool ssl://zec-eu1.nanopool.org:6633 -zwal 0xf9638c00290dfb7bdde8ed8d491528d2c5c7083c -zpsw z 
